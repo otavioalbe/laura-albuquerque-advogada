2d9917c050d70eaf8fe42ee45b69d66d0291b3fd
otavioalbe <otavioalbe@gmail.com>
2025-08-22 10:05:38 -0300

    fix: main container

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,7 +6,7 @@ export default function About() {
     <div className="w-full bg-secondary">
       <Container>
         <div className="flex xl:flex-row flex-col gap-x-10 gap-y-6 !py-10">
-          <div className="flex flex-col md:w-[700px] w-full">
+          <div className="flex flex-col xl:w-[700px] w-full">
             <h1 className="apple-garamond !text-7xl">SOBRE</h1>
             <h2 className="apple-garamond !text-4xl !mt-[-8px]">Dra. Laura Albuquerque</h2>
             <h3 className="!text-2xl !mt-4">
@@ -15,7 +15,7 @@ export default function About() {
             </h3>
             <div className="!text-xl !mt-4 whitespace-pre-line">{useAboutConstants()}</div>
           </div>
-          <div className="flex-1">
+          <div className="flex-1 flex justify-center items-center">
             <img src="../src/assets/images/about.png" alt="about" />
           </div>
         </div>
