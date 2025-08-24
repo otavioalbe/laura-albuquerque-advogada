97cd2b82cfac53f754ebf93896899e98b8734222
otavioalbe <otavioalbe@gmail.com>
2025-08-24 11:23:57 -0300

    fix: banner images position

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,37 +5,42 @@ export default function Banner() {
   const { isMobile, isTablet } = useBreakpoints();
 
   const responsiveLabel = () => {
-    if (isMobile) return 'w-[300px] h-[300px] top-30 left-17';
-    if (isTablet) return 'w-[400px] h-[400px] top-20 left-90';
-    return 'w-[600px] h-[600px] top-0 left-140';
+    if (isMobile) return 'w-[360px] h-[120px] !ml-[8px] !mt-[-300px]';
+    if (isTablet) return 'w-[450px] h-[150px] !ml-[140px] !mt-[-300px]';
+    return 'w-[600px] h-[200px] !ml-[400px] !mt-[-400px]';
   };
 
   const responsiveButton = () => {
-    if (isMobile) return 'top-85 left-54';
-    if (isTablet) return 'top-92 left-137';
-    return 'top-105 left-210';
+    if (isMobile) return '!ml-[70px]';
+    if (isTablet) return '!ml-[250px]';
+    return '!ml-[570px]';
   };
 
   return (
-    <div className="w-full h-[600px] bg-secondary relative">
+    <div className="grid grid-cols-1 grid-rows-1 w-full h-[500px] xl:h-[600px]">
       <img
-        className="w-full h-full object-cover"
         src="../src/assets/images/banner.png"
         alt="banner"
+        className="col-start-1 row-start-1 object-cover h-[500px] xl:h-[600px] w-full md:ml-0"
       />
       <Container>
-        <img
-          className={`absolute  ${responsiveLabel()} object-cover`}
-          src="../src/assets/images/laura-banner.png"
-          alt="banner"
-        />
-        <a
-          href="https://wa.me/5554991184768"
-          target="_blank"
-          className={`whitespace-nowrap apple-garamond body-large absolute ${responsiveButton()} -translate-x-1/2 -translate-y-1/2 border-2 !py-0 !px-10 text-secondary rounded-full`}
-        >
-          Entre em contato
-        </a>
+        <div className="z-10 flex !w-full">
+          <div className="self-center">
+            <img
+              src="../src/assets/images/laura-label.png"
+              alt="banner"
+              className={`${responsiveLabel()} !mb-3`}
+            />
+            <a
+              href="https://wa.me/5554991184768"
+              target="_blank"
+              className={`${responsiveButton()} button-hover whitespace-nowrap apple-garamond body-large border-2 !py-1 !px-10 text-secondary rounded-full`}
+            >
+              {' '}
+              Entre em contato{' '}
+            </a>
+          </div>
+        </div>
       </Container>
     </div>
   );
